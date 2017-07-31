8c50e276d7b46b41c6c6084d299ea6b7ebd4bb5c
minhden <darklantern28146@gmail.com>
2017-07-31 12:41:06 +0700

    Update BIKE.ino

--- BIKE.ino
+++ BIKE.ino
@@ -2,19 +2,19 @@
 #define Tx_Led 9
 #define Aux_Led 10
 #define Button 11
-#define AUX 5
+#define AUX 6
 
 SoftwareSerial mySerial(2, 3); // (2-RX) <<-- Connect TX pin of LoRa  ......  (3-TX) <<-- Connect RX pin of LoRa
 // gnd SET_A and SET_B for Normal Mode (Send and Receive)
 
 
 //int num = 20;
-char data[5] = {0,0,0,0,0};                   // Create an array to store incoming data
+char data[100] = {0,0,0,0,0,0,0};                   // Create an array to store incoming data
 unsigned long wait_time = 3000;
 unsigned long prev_wait_time = 0;
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   mySerial.begin(9600);
   pinMode(Tx_Led, OUTPUT);
   pinMode(Aux_Led, OUTPUT);
@@ -31,31 +31,51 @@ void loop()
     digitalWrite(Tx_Led, HIGH);
     mySerial.println("Bike 1 Lat/Long: 10.729546 / 106.69603299999994");    
     digitalWrite(Tx_Led, LOW);
+    delay (2000);
   }
 
   //Receive the confirmation from the station and print it
   if (mySerial.available() > 1)
   {
-    String input = mySerial.readString();       // Read input from buffer like normal                    
-    input.toCharArray(data,5);                // Convert input type String to store into array data[]
-
-  //If the bike does not receive confirmation resend data
-    unsigned long currentTime = millis ();
-    if ((unsigned long)(currentTime - prev_wait_time) >= wait_time ) 
-    {
-      switch (data [5]) 
+    String input = mySerial.readString();       // Read input from buffer like normal     
+    Serial.println (input);               
+    input.toCharArray(data,100);                // Convert input type String to store into array data[]
+    switch (data [7]) 
       {
         case '0':
           Serial.println ("Transmission failed");
           mySerial.println ("Bike 1 Lat/Long: 10.729546 / 106.69603299999994");
+          break;
         case '1':
           Serial.println ("Data sent");
-      }
+          break;
     }
+
+
+//    if (data [7] =='1') {
+//      Serial.println ("Data sent");
+//    }
+  
+  //If the bike does not receive confirmation resend data
+  
+//    unsigned long currentTime = millis ();
+//    if ((unsigned long)(currentTime - prev_wait_time) >= wait_time ) 
+//    {
+//      switch (data [5]) 
+//      {
+//        case '0':
+//          Serial.println ("Transmission failed");
+//          mySerial.println ("Bike 1 Lat/Long: 10.729546 / 106.69603299999994");
+//          break;
+//        case '1':
+//          Serial.println ("Data sent");
+//          break;
+//      }
+//    }
+    
   //////////////////////////////////////////////////////////
   }
-  
-
+ 
   // LED for checking if the module is receiving or sending data
   if (digitalRead(AUX) == LOW) 
   {
@@ -67,5 +87,5 @@ void loop()
     digitalWrite(Aux_Led, LOW);   
   }
   delay(20);
-} 
+}     
 
